039abf6ab4d3b01106c421a6f4f07d1ae89fade6
ryan-rozario <ryan.rozario1999@gmail.com>
2021-04-24 01:01:20 +0400

    fixed bug with two main statements

--- run.py
+++ run.py
@@ -2,10 +2,10 @@ import os
 
 from src.app import create_app
 
-if __name__ == '__main__':
-  env_name = os.getenv('FLASK_ENV')
-  app = create_app(env_name)
-  # run app
+
+env_name = os.getenv('FLASK_ENV')
+app = create_app(env_name)
+
 if __name__ == '__main__':
   port = os.getenv('PORT')
   port = port or 5000
